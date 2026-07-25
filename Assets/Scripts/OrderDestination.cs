218da456e04dea1c396708a1dc5b821b7d5ee600
Gabrieloide <gabrielfuentes7312@gmail.com>
2026-07-25 06:17:34 -0400

    scene stuff

--- Assets/Scripts/OrderDestination.cs
+++ Assets/Scripts/OrderDestination.cs
@@ -4,10 +4,13 @@ public class OrderDestination : MonoBehaviour
 {
     public bool isActive = false;
     public OrderHolder orderHolder;
-    
-    private void OnTriggerEnter(Collider other)
+
+    private void Update()
     {
-        if (other.CompareTag("Player"))
+        if (!isActive) return;
+
+        Physics.BoxCast(OrderManager.GetPickupPoint(transform), Vector3.one * 0.5f, transform.forward, out RaycastHit hit, transform.rotation, 0.1f);
+        if (hit.collider != null && hit.collider.CompareTag("Player"))
         {
             OrderManager.Instance.OnFinishOrder();
             isActive = false;

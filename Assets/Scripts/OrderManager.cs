32d63ee57bc298c4b171a0544914729b0bfa54b2
Gabrieloide <gabrielfuentes7312@gmail.com>
2026-07-25 06:17:34 -0400

    scene stuff

--- Assets/Scripts/OrderManager.cs
+++ Assets/Scripts/OrderManager.cs
@@ -6,12 +6,13 @@ using System;
 
 public class OrderManager : MonoBehaviour
 {
+    [SerializeField] public Vector3 offset = Vector3.zero;
     private List<OrderHolder> orderTransform;
     private List<OrderDestination> orderDestination;
 
     
-    private Queue<OrderHolder> activesOrderHolder;
-    private Queue<OrderDestination> activesOrderDestination;
+    private Queue<OrderHolder> activesOrderHolder = new Queue<OrderHolder>();
+    private Queue<OrderDestination> activesOrderDestination = new Queue<OrderDestination>();
 
 
     public static Action OnOrderFinished;
@@ -29,27 +30,51 @@ public class OrderManager : MonoBehaviour
         {
             Instance = this;
         }
+        orderTransform = FindObjectsByType<OrderHolder>().ToList();
+        orderDestination = FindObjectsByType<OrderDestination>().ToList();
     }
 
     void Start()
     {
-        orderTransform = FindObjectsByType<OrderHolder>().ToList();
-        orderDestination = FindObjectsByType<OrderDestination>().ToList();
+
         playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         AddOrder();
     }
     private void Update()
     {
+        if (activesOrderHolder.Count == 0)
+        {
+            Debug.LogWarning("No active orders available.");
+            return;
+        }
         if(Vector3.Distance(playerTransform.position, activesOrderHolder.Last().transform.position) < 1f && activesOrderHolder.Count == 1)
         {
             AddOrder();
         }
     }
 
+    public static Vector3 GetPickupPoint(Transform target)
+    {
+        return target.position + target.TransformDirection(Vector3.forward + Instance.offset);
+    }
+
+    private static Mesh capsuleGizmoMesh;
+    private static void DrawWireCapsule(Vector3 position, Quaternion rotation, Vector3 scale)
+    {
+        if (capsuleGizmoMesh == null)
+        {
+            capsuleGizmoMesh = Resources.GetBuiltinResource<Mesh>("Capsule.fbx");
+        }
+        Gizmos.DrawWireMesh(capsuleGizmoMesh, position, rotation, scale);
+    }
+
     public void AddOrder()
     {
-        activesOrderHolder.Enqueue(orderTransform[UnityEngine.Random.Range(0, orderTransform.Count)]);
-        activesOrderDestination.Enqueue(orderDestination[UnityEngine.Random.Range(0, orderDestination.Count)]);
+        var availableOrderHolders = orderTransform[UnityEngine.Random.Range(0, orderTransform.Count)];
+        activesOrderHolder.Enqueue(availableOrderHolders);
+
+        var availableOrderDestinations = orderDestination[UnityEngine.Random.Range(0, orderDestination.Count)];
+        activesOrderDestination.Enqueue(availableOrderDestinations);
 
         var currentOrderHolder = activesOrderHolder.Last();
         var currentOrderDestination = activesOrderDestination.Last();
@@ -74,20 +99,29 @@ public class OrderManager : MonoBehaviour
     }
     void OnDrawGizmos()
     {
+
+
+        if(orderTransform == null) return;
         foreach (var order in orderTransform)
         {
             if(order == null) continue;
+
+            Gizmos.color = Color.green;
+            DrawWireCapsule(GetPickupPoint(order.transform), order.transform.rotation, Vector3.one * 8f);
+
             if(order.isActive == false) continue;
 
             Gizmos.color = Color.green;
-            Gizmos.DrawWireSphere(order.transform.position, 0.5f);
+            Gizmos.DrawWireSphere(GetPickupPoint(order.transform), 0.5f);
 
             if(order.orderDestination == null) continue;
             Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(order.orderDestination.transform.position, 0.5f);
+            Gizmos.DrawWireSphere(GetPickupPoint(order.orderDestination.transform), 0.5f);
 
             Gizmos.color = Color.turquoise;
-            Gizmos.DrawLine(order.transform.position, order.orderDestination.transform.position);
+            Gizmos.DrawLine(GetPickupPoint(order.transform), GetPickupPoint(order.orderDestination.transform));
+
+
         }
     }
 }
